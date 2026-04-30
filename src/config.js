e3b181405d00f9201d89957420d12c3ee136cb4f
Maximiliano Revilla <mrevilla@fvet.uba.ar>
2026-04-30 18:51:36 -0300

    Final Google Drive configuration using @fvet account with fixed permissions script

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 // Archivo de configuración para servicios externos
 export const CONFIG = {
   // Nueva URL de la implementación (Versión Super Robusta)
-  GOOGLE_DRIVE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzlVRbDQccusAHlwuebePt9aL23rtGcFs8D1VKy-sUAOPdpmCEgSCakAVKKGqaygfEe/exec",
+  GOOGLE_DRIVE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxG-t-eZbugxmvXxCBdh55Ugym_GPlmgvqPMlcb1DXIQQhgSXznPehSON7ibgjYwBSFnA/exec",
   
   // ID de la carpeta de Drive: fungitrack
-  GOOGLE_DRIVE_FOLDER_ID: "1Ww1Pd6ZG0s4j94BIu6z9AvZSpfuTBaZ0" 
+  GOOGLE_DRIVE_FOLDER_ID: "1cr19_4UGB8MFOCZNg4wqam0erd_DkwFC" 
 };
