41aa51b54551870e25a259c3109fb053d63c6e48
Maximiliano Revilla <mrevilla@fvet.uba.ar>
2026-04-30 18:01:50 -0300

    Update Google Apps Script URL for Drive upload

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // Archivo de configuración para servicios externos
 export const CONFIG = {
   // Nueva URL de la implementación (Versión Super Robusta)
-  GOOGLE_DRIVE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzofvXycDJ8xlQdOLiNaWTGqr8saBvYQLrZP3hSKSB-X2dJM8ejJjAovm0Go5oPh3BiOg/exec",
+  GOOGLE_DRIVE_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbw9v-HE889weZo4rdQnxenhGdzSR3DBphNriSKhUEUowEvDFbIrVhpvxnU_OrDKEYuJ/exec",
   
   // ID de la carpeta de Drive: fungitrack
   GOOGLE_DRIVE_FOLDER_ID: "1cr19_4UGB8MFOCZNg4wqam0erd_DkwFC" 
